e6c838433065d416e6c66543b199e893fc7725b8
Hannah Maratea <maratea101@gmail.com>
2022-11-13 14:11:07 -0500

    commit tree ed7165dafb58bcd443064095eb3cd23ee7ce949b

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// scuberGreetingForFeet()
+function scuberGreetingForFeet (feet) {
+  if (feet <= 400) {  
+    return 'This one is on me!';  
+  } else if (feet > 400 && feet < 2000) {
+    return 'That will be twenty bucks.';  
+  } else if (feet > 2000 && feet < 2449) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (feet > 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+// ternaryCheckCity()
+function ternaryCheckCity (city) {
+  console.log(city);
+  if (city === 'NYC') {
+    return 'Ok, sounds good.'
+  } else {
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// switchOnCharmFromTip()
+// switch switchOnCharmFromTip (response) {
+//   case 0:
+//     response = 'Thank you so much.';
+//     break;
+//   case 1:
+//     response = 'Thank you.';
+//     break;
+//   default:
+//     response = 'Bye';
+//     break;
+// }
+
+function switchOnCharmFromTip (tip) {
+  console.log(tip);
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    case 'thanks for everything':
+      return 'Bye.';
+      break;
+  }
 }
